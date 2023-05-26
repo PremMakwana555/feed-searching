533ed192740daa388f8de9f9d2e7d520ec60553e
Prem Makwana <69688220+PremMakwana555@users.noreply.github.com>
2023-05-26 16:24:54 +0530

    initial commit

---
+++ src/main/java/com/kodo/feedsearching/FeedSearchingApplication.java
@@ -0,0 +1,13 @@
+package com.kodo.feedsearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedSearchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedSearchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodo/feedsearching/FeedSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodo.feedsearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedSearchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
